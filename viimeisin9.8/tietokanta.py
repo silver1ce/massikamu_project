7874d2af599b77eedaa83bd05bd9af104ac113a7
ilonapajunen <127730870+ilonapajunen@users.noreply.github.com>
2023-08-09 23:07:12 +0300

    Add files via upload

---
+++ viimeisin9.8/tietokanta.py
@@ -0,0 +1,48 @@
+import pymongo
+import certifi
+import datetime
+import massikamusovellus
+# Connect to MongoDB
+
+connection = pymongo.MongoClient("mongodb+srv://temp:massikamu@cluster0.qn6cv7r.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+db = connection["MassiKamu"]
+expense_collection = db["expenses"]
+
+def yhdista_mongoon():
+    try:
+        CONNECTION_STRING="mongodb+srv://temp:massikamu@cluster0.qn6cv7r.mongodb.net/?retryWrites=true&w=majority"
+        connection = pymongo.MongoClient(CONNECTION_STRING)
+        print("yhteys ok")
+        return connection["MassiKamu"]
+
+    except Exception:
+    
+        print("Ei voida yhdistää Mongoon")
+        #lisätään vielä käyttäjälle viesti 
+
+def tallenna_tiedot(nimi:str, summa:float, paivamaara:datetime):
+    expense_collection.insert_one(
+        {
+            "nimi": nimi,
+            "summa":summa,
+            "paivamaara": paivamaara
+        }
+    )
+
+
+
+def nayta_tiedot():
+    tiedot = []
+    for rivi in expense_collection.find():
+        tiedot.append(
+            rivi["nimi"] + 
+            ": " +
+            str(rivi["summa"])+ 
+            ", " +
+            (rivi ["paivamaara"]).strftime("%d.%m.%Y %H:%M")
+
+        )
+    
+    return tiedot
+
+db = yhdista_mongoon()
