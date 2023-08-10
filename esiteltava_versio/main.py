05763e0bc6b96a36f1287dc8d037fe47b6e8602a
ilonapajunen <127730870+ilonapajunen@users.noreply.github.com>
2023-08-10 12:32:15 +0300

    Add files via upload

---
+++ esiteltava_versio/main.py
@@ -0,0 +1,12 @@
+
+import massikamusovellus
+import tietokanta
+
+
+
+if __name__ == '__main__':
+    
+    massikamusovellus.MassiKamuApp().run()
+    
+# Close the MongoDB connection
+#ietokanta.expense_collection.close()
