3348f1aa14fa621f74d76ef64e154beed9b207d0
Jahangir Alam <jahangir_alam_99@hotmail.com>
2023-08-20 13:52:52 +0300

    Updated pie-chart

--- massikamusovellus.py
+++ massikamusovellus.py
@@ -10,11 +10,13 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 
+
 #ensimmäinen sivu, joka avautuu:
 class MainWindow(Screen):
     def __init__(self, **kw):
         super(MainWindow, self).__init__(**kw)
         #luodaan layout pääsivulle
+        
         self.layout = BoxLayout(orientation='vertical')
         
         #luodaan kategorian ja summan syöttömahdollisuudet
@@ -23,6 +25,7 @@ class MainWindow(Screen):
 
         #luodaan buttonit, joilla voi lisätä tiedot ja siirtyä toiselle sivulle
         self.add_expense_button = Button(text='Lisää kulu', on_press=self.add_expense)
+        self.delete_expense_button = Button(text='Poista viimeinen kulu', on_press=self.delete_last_expense)
 
         #luodaan näytä-button
         self.show_button = Button(text='Näytä tiedot', on_press=self.switch_to_reportwindow)
@@ -37,25 +40,34 @@ class MainWindow(Screen):
         self.layout.add_widget(self.category_input)
         self.layout.add_widget(self.amount_input)
         self.layout.add_widget(self.add_expense_button)
+        self.layout.add_widget(self.delete_expense_button)
         #self.layout.add_widget(self.remove_expense_button)
         self.layout.add_widget(self.show_button)
         
         #lopuksi lisätään layout tähän mainwindowiin
         self.add_widget(self.layout)
 
+    #yritys popupin tekoon, ei onnistunut vielä
+    # def open_popup(self, instance):
+    #     #lisätään popup-ikkuna, joka kertoo, että ei saatu yhteyttä tietokantaan
+    #             lisattyPopup = Popup(title="Ei saatu yhteyttä tietokantaan...", 
+    #             content=Label(text="Tarkista internetyhteytesi."), 
+    #             size_hint=(0.7, 0.2))
+    #             lisattyPopup.open()
+
     def add_expense(self, instance):
 
         #jos käyttäjä ei kirjoita mitään, ja yrittää painaa Lisää kulu-nappia,
         #mitään ei tapahdu
         if len(self.category_input.text) == 0:
-            print("ei mitää ")
+            print("ei mitään.")
         #jos sen sijaan käyttäjä kirjoittaa jotakin kategoriakohtaan, kokeillaan 
         #seuraavaksi, onhan käyttäjä syöttänyt Summa-kohtaan jonkin luvun, joka on
         #muunnettavissa floatiksi. jos tämäkin onnistuu, tiedot lisätään tietokantaan
         #päivämäärä- ja aikatietojen kera
         elif len(self.category_input.text) > 0:
             category = self.category_input.text
-            print("suurempi")
+            
             try:
                 amount = float(self.amount_input.text)
                 expense = {
@@ -81,7 +93,36 @@ class MainWindow(Screen):
             
             self.category_input.text = ''
             self.amount_input.text = ''
+    
+    def delete_last_expense(self, instance):
+        #tämä funktio vain kysyy varmistuksen poistolle. Popup widgettiin
+        #ei saanut lisättyä kuin yhden buttonin, niin laitoin vain kylläbuttonin
+        #tämä pitäisi korjata tulevaisuudessa
+        self.varmistusPopup = Popup(title="Oletko varma, että haluat poistaa tämän?",  
+        size_hint=(0.7, 0.2))
+        kyllaButton = Button(text="Kyllä", on_press=self.delete_from_Mongo)
+       # eiButton = Button(text="Ei", on_press=poisto1Popup.dismiss)
+        self.varmistusPopup.add_widget(kyllaButton)
+        #poisto1Popup.add_widget(eiButton)
+        self.varmistusPopup.open()
+
+    def delete_from_Mongo(self,instance):
+        #jos käyttäjä on varmistanut poiston, tämä funktio poistaa tiedon tietokannasta
+        #suljetaan varmistuspopup
+        self.varmistusPopup.dismiss()
+        #etsii viimeisen kulun ja sen id-numeron
+        viimeinen = tietokanta.expense_collection.find_one(sort=[('_id', -1)])
+        viimeinen_id = viimeinen['_id']
+        #poistetaan tietokannasta viimeinen kulu
+        tietokanta.expense_collection.delete_one({'_id': viimeinen_id})
+        #lisätään popup-ikkuna, joka kertoo, että tiedot on poistettu onnistuneesti
+        poistoPopup = Popup(title="Onnistui!", 
+        content=Label(text="Viimeinen kulu on nyt poistettu."), 
+        size_hint=(0.7, 0.2))
         
+        poistoPopup.open()
+
+
     def switch_to_reportwindow(self, instance):
         self.manager.current = 'reportwindow'
 
@@ -96,8 +137,8 @@ class ReportWindow(Screen):
         self.label2 = Label(text='Massikamutietosi', size_hint=(1, 0.1))
         
         #luodaan toimintobuttonit
-        self.all_the_information_button = Button(text="Näytä kaikki tiedot", on_press=self.generate_information, size_hint=(1, 0.4))
-        self.generate_report_button = Button(text='Luo raportti', on_press=self.generate_report, size_hint=(1, 0.4))
+        self.all_the_information_button = Button(text="Taulukko", on_press=self.generate_information, size_hint=(1, 0.4))
+        self.generate_report_button = Button(text='Diagrammi', on_press=self.generate_report, size_hint=(1, 0.4))
         
         
         #luodaan navigointibutton
@@ -124,10 +165,39 @@ class ReportWindow(Screen):
     def switch_to_informationwindow(self):
         self.manager.current = 'informationwindow'
     
+    def current_month_in_Finnish(self):
+        #tarkastetaan, mikä kuukausi nyt on, ja käännetään sanakirjan avulla se suomeksi
+        current_date = datetime.date.today()
+        current_month = current_date.strftime("%B")
+        kuukaudet_suomeksi =  {"January" : "tammikuu", "February":"Helmikuu", "March": "Maaliskuu", "Abril":"Huhtikuu", "May": "Toukokuu", "June":"Kesäkuu", "July":"Heinäkuu", "August" : "Elokuu", "September": "Syyskuu", "October":"Lokakuu", "November":"Marraskuu", "December":"Joulukuu"}
+        kuukausi = kuukaudet_suomeksi[current_month]
+        return kuukausi
+    
+    def query_this_month(self):
+        
+        # käynnissä olevan kuukauden tiedot
+        current_date = datetime.datetime.today()
+        first_day_of_month = datetime.datetime(current_date.year, current_date.month, 1)
+        #haetaan tietokannasta vain tämän kuukauden tiedot
+        query = {"date": {"$gte": first_day_of_month, "$lte": current_date}}
+        kuukausi = tietokanta.expense_collection.find(query)
+        return kuukausi
+
+    def total_costs(self):
+        # käynnissä olevan kuukauden kulut 
+        kuukausi = ReportWindow.query_this_month(self)
+        yhteensa = 0
+        for row in kuukausi:
+            #print(row)
+            yhteensa += float(row["amount"])
+        return yhteensa
+            
     def generate_report(self, instance):
+        
         current_date = datetime.date.today()
         first_day_of_month = datetime.datetime(current_date.year, current_date.month, 1)
 
+       
         pipeline = [
             {
                 "$match": {
@@ -145,24 +215,42 @@ class ReportWindow(Screen):
         
         categories = []
         amounts = []
-    
+        lista = []
+
         for row in result:
-            categories.append(row["_id"])
-            amounts.append(row["total_amount"])
+            rivi = []
+            rivi.append(row["_id"])
+            rivi.append(row["total_amount"])
+            lista.append(rivi)
+
+        #järjestetään tiedot aakkosjärjestykseen
+        lista = sorted(lista)
+
+        for row in lista:
+            categories.append(row[0])
+            amounts.append(row[1])
 
         # Generate bar chart
-        plt.bar(categories, amounts)
-        plt.xlabel('Kategoria')
-        plt.ylabel('Summa')
-        plt.title('Kuukausiraportti')
-        plt.xticks(rotation='vertical')
-        plt.tight_layout()
-        #tämä tallentaa vain kerran tämän raportin, eikä uudestaan, kun nappia painetaan
-        #tästä syystä raportti ei päivity, kun lisätään uusia ostoksia
-        plt.savefig('report.png')  # Save the chart as an image
+        # plt.bar(categories, amounts)
+        # plt.xlabel('Kategoria')
+        # plt.ylabel('Summa')
+        # plt.title(self.current_month_in_Finnish() + " yhteensä: " + str(self.total_costs()) + " euroa" )
+        # plt.xticks(rotation='vertical')
+        # plt.tight_layout()
+        # #tämä tallentaa vain kerran tämän raportin, eikä uudestaan, kun nappia painetaan
+        # #tästä syystä raportti ei päivity, kun lisätään uusia ostoksia
+        # plt.savefig('report.png')  # Save the chart as an image
         
-        plt.close()  # Close the figure
+        # plt.close()  # Close the figure
         
+        # Create a pie chart
+        plt.pie(amounts, labels=categories, autopct='%1.1f%%', startangle=140)
+        plt.axis('equal')  # Equal aspect ratio ensures that the pie is drawn as a circle.
+        plt.title(self.current_month_in_Finnish() + " yhteensä: " + str(self.total_costs()) + " euroa" )
+        plt.savefig('report.png')
+        plt.close()
+
+
         
         # self.report_label.text = 'Monthly Expense Report'
         # self.report_label.canvas.ask_update()  # Update the label to show the new text
@@ -171,11 +259,12 @@ class ReportWindow(Screen):
        # self.add_widget(Button(text="Siirry Screen 2:een", on_press=self.switch_to_screen2))
 
         #lopuksi vaihdetaan seuraavalle sivulle
+        
         self.switch_to_imagewindow()
 
     def generate_information(self, instance):
-        #luodaan lista kaikista tiedoista
-    
+        #luodaan lista kaikista tämän kuun tiedoista
+        kuukausi = self.query_this_month()
         data = tietokanta.expense_collection.find()
         tiedot = []
         ekarivi = []
@@ -184,15 +273,14 @@ class ReportWindow(Screen):
         ekarivi.append("Päivämäärä")
         tiedot.append(ekarivi)
         
-        for row in data: 
+        for row in kuukausi: 
             rivi = []
             rivi.append(row["category"])
             rivi.append(row["amount"] )
             rivi.append(row["date"])
-           # rivi = [(f'{row["category"]}        {row["amount"]}         {row["date"]}')]
-            print(rivi)
             tiedot.append(rivi)
-        print(tiedot)   
+        #kulut aakkosjärjestykseen
+        tiedot = sorted(tiedot)
         # Luodaan matplotlib-kuva
         fig, ax = plt.subplots()
         ax.axis('off')  # Poista x- ja y-akselit
@@ -213,15 +301,18 @@ class ImageWindow(Screen):
     def __init__(self, **kw):
         super(ImageWindow, self).__init__(**kw)
         #luodaan seuraavan sivun layout
+        
         self.layout3 = BoxLayout(orientation='vertical')
-
+        
         #otsikko seuraavalle sivulle
         self.label3 = Label(text='Kuukausiraportti', size_hint=(1, 0.1))
 
         #luodaan image
         #tällä hetkellä näkyy vain juuri sovelluksen avaushetkellä ollut tilanne, eikä kuva päivity 
         #kun lisätään kuluja
+        
         self.image = Image(source="report.png", size_hint=(1, 0.8))
+        
         #luodaan button
         self.button3 = Button(text="Takaisin", on_press=self.switch_to_reportwindow, size_hint=(1, 0.1))
         
@@ -242,9 +333,10 @@ class InformationWindow(Screen):
         super(InformationWindow, self).__init__(**kw)
         #luodaan seuraavan sivun layout
         self.layout4 = BoxLayout(orientation='vertical')
-
+        kuukausi = ReportWindow.current_month_in_Finnish(self)
+        kulut_yhteensa = ReportWindow.total_costs(self)
         #otsikko seuraavalle sivulle
-        self.label4 = Label(text='Kaikki tietosi', size_hint=(1, 0.1))
+        self.label4 = Label(text= kuukausi +"n kaikki kulut yhteensä: " + str(kulut_yhteensa) + " euroa", size_hint=(1, 0.1))
 
         #luodaan image
         #tällä hetkellä näkyy vain juuri sovelluksen avaushetkellä ollut tilanne, eikä kuva päivity 
@@ -270,10 +362,6 @@ class InformationWindow(Screen):
     def switch_to_reportwindow(self, instance):
         self.manager.current = 'reportwindow'
 
-            
-#Screenmanagerluokan luonti
-#class WindowManager(ScreenManager):
-    #pass
 
 
 class MassiKamuApp(App):
